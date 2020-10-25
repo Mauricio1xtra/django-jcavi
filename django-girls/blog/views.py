829ba0c9d1b555fea7a8290a169666b082d99c1d
Mauricio <bestsound2u@gmail.com>
2020-10-25 13:50:49 -0300

    Adicionado templates e views
    - Projeto django girls

--- django-girls/blog/views.py
+++ django-girls/blog/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
-
-# Create your views here.
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
     return render(request, 'blog/post_list.html', {'posts': posts})
-    
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post' :post})
+
+def post_new(request):
+    return _post_new_edit(request)
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return _post_new_edit(request, post)
+
+
+def _post_new_edit(request, post=None):
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post) if post else PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post) if post else PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
